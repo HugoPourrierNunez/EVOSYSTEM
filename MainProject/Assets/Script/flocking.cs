e57f1831b2fcff015498396a1adb6ac103f50f99
anthonybazelle <a.bazelle1@laposte.net>
2017-03-16 23:10:03 +0100

    Boids dans MainProject

---
+++ MainProject/Assets/Script/flocking.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class flocking : MonoBehaviour {
+
+    private float speed = 0.001f;
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    float neighbourDistance = 3.0f;
+
+    [SerializeField]
+    GameObject obj;
+
+    Matrix4x4[] matrix;
+
+    [SerializeField]
+    MeshFilter myMeshFilter;
+
+    [SerializeField]
+    Material myMaterial;
+
+    bool turning = false;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(0.5f, 1);
+        matrix = new Matrix4x4[2] { obj.transform.localToWorldMatrix, this.transform.localToWorldMatrix };
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Vector3.Distance(transform.position, globalFlocking.origin) >= globalFlocking.tankSize)
+        {
+            turning = true;
+        }
+        else
+        {
+            turning = false;
+        }
+
+        if (turning)
+        {
+            // On indique ici Vector3.zero car o n suppose que le centre de la scène se trouve en (0,0,0)
+            Vector3 direction = globalFlocking.origin - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+
+            speed = Random.Range(0.5f, 1);
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1)
+            {
+                ApplyRules();
+            }
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);
+       // Graphics.DrawMesh(myMeshFilter.mesh, transform.worldToLocalMatrix, );
+        Graphics.DrawMeshInstanced(myMeshFilter.mesh, 0, myMaterial, matrix, matrix.Length, null, ShadowCastingMode.On, true, 0, null);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = globalFlocking.allBoids;
+
+        Vector3 vcentre = globalFlocking.origin;
+        Vector3 vavoid = globalFlocking.origin;
+        float gSpeed = 0.1f;
+
+        Vector3 goalPos = globalFlocking.goalPos;
+
+        float dist;
+
+        int groupSize = 0;
+
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+
+                if (dist <= neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if (dist < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    flocking anotherFlocking = go.GetComponent<flocking>();
+                    gSpeed = gSpeed + anotherFlocking.speed;
+                }
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ MainProject/Assets/Script/globalFlocking.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalFlocking : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject boidsPrefab;
+
+    [SerializeField]
+    private GameObject goalPrefab;
+
+    // taille de la zone de deplacement des boids sur les 3 dimensions (un cube en fait)
+    public static int tankSize = 5;
+
+    public static Vector3 origin = new Vector3(20, 12.6f, -5);
+    static int numBoids = 60;
+    public static GameObject[] allBoids = new GameObject[numBoids];
+    public static Vector3 goalPos = origin;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < numBoids; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(origin.x - tankSize, origin.x + tankSize),
+                                      Random.Range(origin.y - tankSize, origin.y + tankSize),
+                                      Random.Range(origin.z - tankSize, origin.z + tankSize)
+                                      );
+            allBoids[i] = (GameObject)Instantiate(boidsPrefab, pos, Quaternion.identity);
+        }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.Range(0, 10000) < 50)
+        {
+            goalPos = new Vector3(Random.Range(-tankSize + origin.x, tankSize - origin.x),
+                                      Random.Range(-tankSize + origin.y, tankSize - origin.y),
+                                      Random.Range(-tankSize + origin.z, tankSize - origin.z)
+                                );
+            goalPrefab.transform.position = goalPos;
+        }
+	}
+}
